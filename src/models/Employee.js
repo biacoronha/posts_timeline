1ce76286d2ad09f849fe2bc8409dc0528164be57
Beatriz Coronha <biacoronha@gmail.com>
2021-12-11 21:32:19 -0300

    add user model

---
+++ src/models/Employee.js
@@ -0,0 +1,27 @@
+export default class Employee {
+    
+    id;
+    username;
+    phone;
+    role;
+    name;
+
+    constructor() {}
+
+    setUsername(username){
+        this.username = username
+    }
+
+    setPhone(phone){
+        this.phone = phone
+    }
+
+    setRole(role){
+        this.role = role
+    }
+
+    setName(name){
+        this.name = name
+    }
+
+}
